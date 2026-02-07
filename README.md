22e1d27dc8a352379fa030f65f061a22122a9c01
Dan Knutson <19314778+Giesch@users.noreply.github.com>
2026-02-07 18:29:24 -0500

    lerp shape to mouse

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Assets
 
-- Beethoven's `Eroica` ('heroic' symphony) 3rd movement ('Scherzo Allegro vivace')  
+- Beethoven's `Eroica` (or 'heroic' symphony) 3rd movement ('Scherzo Allegro vivace')  
   as performed by the Czech National Symphony Orchestra in 2012 (public domain)  
   https://imslp.org/wiki/Symphony_No.3%2C_Op.55_(Beethoven%2C_Ludwig_van)
+- photo of the original score, from wikipedia (public domain)  
+  https://en.wikipedia.org/wiki/Symphony_No._3_%28Beethoven%29#/media/File:Eroica_Beethoven_title.jpg
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,64 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 use bevy_seedling::prelude::*;
 
 fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(SeedlingPlugin::default())
-        .add_systems(Startup, play_music)
+        .add_systems(Startup, (play_scherzo, spawn_camera, spawn_quill))
+        .add_systems(FixedUpdate, (lerp_quill_to_mouse,))
         .run()
 }
 
-fn play_music(mut commands: Commands, server: Res<AssetServer>) {
-    const BEETHOVEN: &str = "audio/03_Scherzo_Allegro_vivace.flac";
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
 
-    commands.spawn(SamplePlayer::new(server.load(BEETHOVEN)));
+#[derive(Component)]
+struct Quill;
+
+fn spawn_quill(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let mesh = meshes.add(Annulus::new(25.0, 50.0));
+    let color = Color::hsl(360.0, 0.95, 0.7);
+
+    commands.spawn((
+        Quill,
+        Mesh2d(mesh),
+        MeshMaterial2d(materials.add(color)),
+        Transform::default(),
+    ));
+}
+
+fn lerp_quill_to_mouse(
+    window: Single<&Window, With<PrimaryWindow>>,
+    camera_query: Single<(&Camera, &GlobalTransform)>,
+    mut quills: Query<&mut Transform, With<Quill>>,
+) {
+    const QUILL_SPEED: f32 = 0.1;
+
+    let Some(mouse_pos_screen_space) = window.cursor_position() else {
+        return;
+    };
+
+    let (camera, camera_transform) = *camera_query;
+    let Ok(mouse_pos) = camera.viewport_to_world_2d(camera_transform, mouse_pos_screen_space)
+    else {
+        return;
+    };
+
+    for mut quill_transform in &mut quills {
+        let quill_pos = quill_transform.translation.xy();
+        let moved = quill_pos.lerp(mouse_pos, QUILL_SPEED);
+        quill_transform.translation.x = moved.x;
+        quill_transform.translation.y = moved.y;
+    }
+}
+
+fn play_scherzo(mut commands: Commands, server: Res<AssetServer>) {
+    const SCHERZO: &str = "audio/03_Scherzo_Allegro_vivace.flac";
+    commands.spawn(SamplePlayer::new(server.load(SCHERZO)));
 }
