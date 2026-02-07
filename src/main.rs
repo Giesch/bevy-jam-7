1f834d7f59edf81bc1f0259077c187cb0d562c2e
Dan Knutson <19314778+Giesch@users.noreply.github.com>
2026-02-07 17:12:31 -0500

    play beethoven

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
+use bevy_seedling::prelude::*;
 
 fn main() -> AppExit {
-    App::new().add_plugins(DefaultPlugins).run()
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(SeedlingPlugin::default())
+        .add_systems(Startup, play_music)
+        .run()
+}
+
+fn play_music(mut commands: Commands, server: Res<AssetServer>) {
+    const BEETHOVEN: &str = "audio/03_Scherzo_Allegro_vivace.flac";
+
+    commands.spawn(SamplePlayer::new(server.load(BEETHOVEN)));
 }
