9cca512e0f0a062f477adfe6bf740cfe046e5c6f
Dan Knutson <19314778+Giesch@users.noreply.github.com>
2026-02-07 19:30:57 -0500

    replace dx with inline_tweak

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 use bevy_seedling::prelude::*;
+use inline_tweak::tweak;
 
 fn main() -> AppExit {
     App::new()
@@ -16,6 +18,11 @@ fn spawn_camera(mut commands: Commands) {
 
 #[derive(Component)]
 struct Quill;
+
+fn play_scherzo(mut commands: Commands, server: Res<AssetServer>) {
+    const SCHERZO: &str = "audio/03_Scherzo_Allegro_vivace.flac";
+    commands.spawn(SamplePlayer::new(server.load(SCHERZO)));
+}
 
 fn spawn_quill(
     mut commands: Commands,
@@ -38,7 +45,7 @@ fn lerp_quill_to_mouse(
     camera_query: Single<(&Camera, &GlobalTransform)>,
     mut quills: Query<&mut Transform, With<Quill>>,
 ) {
-    const QUILL_SPEED: f32 = 0.1;
+    let quill_speed = tweak!(0.1);
 
     let Some(mouse_pos_screen_space) = window.cursor_position() else {
         return;
@@ -52,13 +59,8 @@ fn lerp_quill_to_mouse(
 
     for mut quill_transform in &mut quills {
         let quill_pos = quill_transform.translation.xy();
-        let moved = quill_pos.lerp(mouse_pos, QUILL_SPEED);
+        let moved = quill_pos.lerp(mouse_pos, quill_speed);
         quill_transform.translation.x = moved.x;
         quill_transform.translation.y = moved.y;
     }
-}
-
-fn play_scherzo(mut commands: Commands, server: Res<AssetServer>) {
-    const SCHERZO: &str = "audio/03_Scherzo_Allegro_vivace.flac";
-    commands.spawn(SamplePlayer::new(server.load(SCHERZO)));
 }
